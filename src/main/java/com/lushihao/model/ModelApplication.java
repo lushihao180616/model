ddb7b0b63f65bb3bed5323c39d1e97fcdc64244e
lushihao <12345678>
2020-01-02 11:29:23 +0800

    初始化

--- src/main/java/com/lushihao/model/ModelApplication.java
+++ src/main/java/com/lushihao/model/ModelApplication.java
@@ -1,9 +1,11 @@
 package com.lushihao.model;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.springboot.demo.dao")
 public class ModelApplication {
 
 	public static void main(String[] args) {
