897b3c06f9e31a4209553aa59c478e344f3528eb
lushihao <lushihaomb@qq.com>
2020-01-03 20:07:55 +0800

    chushihua

--- src/main/java/com/lushihao/model/ModelApplication.java
+++ src/main/java/com/lushihao/model/ModelApplication.java
@@ -1,11 +1,10 @@
 package com.lushihao.model;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.lushihao.model.dao")
+//@MapperScan("com.lushihao.model.dao")
 public class ModelApplication {
 
     public static void main(String[] args) {
